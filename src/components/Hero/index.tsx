d2c7f879657b5c6fbe497d2f967be722d0cef79d
rodrigopenafurt <rodfpar@hotmail.com>
2024-02-28 16:48:22 -0300

    adaptando para styled-components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
-import styles from './Hero.module.css'
+//import styles from './Hero.module.css'
+import { FormHero, TitleHero, DivHero } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <FormHero>
+    <DivHero>
+      <TitleHero>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </TitleHero>
+    </DivHero>
+  </FormHero>
 )
 
 export default Hero
