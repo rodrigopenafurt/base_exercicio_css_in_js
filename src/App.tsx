7555b17ea5f9445bf8f19fcb619b4a70a65ad0c9
rodrigopenafurt <rodfpar@hotmail.com>
2024-02-28 20:06:46 -0300

    atualizacao prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-// import './global.css'
-
 function App() {
   return (
     <>
